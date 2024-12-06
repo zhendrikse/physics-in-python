10bb5147a4dcca01c00e3c50b62626d5572f4c01
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-12-06 12:43:20 +0100

    Update from Trinket

--- src/trinket/elastic_collision.py
+++ src/trinket/elastic_collision.py
@@ -19,7 +19,7 @@ class Sphere:
   def has_collided_with(self, other):
     return mag(self._distance_to(other)) < (self._sphere.radius + other._sphere.radius)
   
-  def update_position_and_momentum(self, force, dt):
+  def move(self, force, dt):
     self._sphere.p += force * dt
     self._sphere.pos += self._sphere.p * dt / self._sphere.mass
 
@@ -27,7 +27,7 @@ class Sphere:
      return self._sphere.p
   
   def kinetic_energy(self):
-     return mag(self.momentum())**2 / (2* self._sphere.mass)
+     return mag(self.momentum())**2 / ( 2* self._sphere.mass)
   
   def force_between(self, other):
       k = 101
@@ -40,7 +40,6 @@ sphere_B = Sphere(0.1, vector( .2, .0,  0), vector(0,  0 ,0), 0.05, color.cyan)
 print("initial momentum = " + str(sphere_A.momentum() + sphere_B.momentum()) + " kg*m/s")
 print("initial K = " + str(sphere_A.kinetic_energy() + sphere_B.kinetic_energy()) + " Joules")
 
-dt = 0.01
 for timestep in range(0, 350):
   rate(100)
   force_BA = zero_force
@@ -48,8 +47,8 @@ for timestep in range(0, 350):
   if sphere_A.has_collided_with(sphere_B):
     force_BA = sphere_A.force_between(sphere_B)
 
-  sphere_A.update_position_and_momentum(force_BA, dt)
-  sphere_B.update_position_and_momentum(-force_BA, dt)
+  sphere_A.move(force = force_BA, dt = 0.01)
+  sphere_B.move(force = -force_BA, dt = 0.01)
 
 print("final momentum = " + str(sphere_A.momentum() + sphere_B.momentum()) + " kg*m/s")
 print("final K = " + str(sphere_A.kinetic_energy() + sphere_B.kinetic_energy()) + " Joules")
