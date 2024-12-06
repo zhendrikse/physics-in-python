979956b925592c048784d4183c2787ae8651f335
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-12-06 17:42:51 +0100

    Added tests

--- test/ball_test.py
+++ test/ball_test.py
@@ -1,5 +1,5 @@
 import pytest
-from hamcrest import assert_that, equal_to, is_
+from hamcrest import assert_that, equal_to, is_, is_not
 from vpython import vector, color, mag
 from src.toolbox.ball import Ball
 
@@ -27,10 +27,25 @@ class TestBall:
       ball_2 = BallTestDouble(position=vector(1, 2, 3))
       assert_that(ball_at_origin.distance_to(ball_2), equal_to(vector(1, 2, 3)))
 
-  def test_no_collision(self, ball_at_origin):
+  def test_no_collision_for_non_colliding_balls(self, ball_at_origin):
      ball_2 = BallTestDouble(position=vector(2 * ball_at_origin._ball.radius, 0.0, 0.0))
      assert_that(ball_at_origin.has_collided_with(ball_2), is_(False))
 
-  def test_collision(self, ball_at_origin):
+  def test_collision_for_colliding_balls(self, ball_at_origin):
      ball_2 = BallTestDouble(position=vector(2 * ball_at_origin._ball.radius - 0.001, 0, 0))
      assert_that(ball_at_origin.has_collided_with(ball_2), is_(True))
+
+  def test_force_is_zero_between_non_colliding_balls(self, ball_at_origin):
+     ball_2 = BallTestDouble(position=vector(2 * ball_at_origin._ball.radius, 0.0, 0.0))
+     assert_that(ball_at_origin.force_between(ball_2), equal_to(vector(0, 0, 0)))
+
+  def test_force_is_not_zero_between_non_colliding_balls(self, ball_at_origin):
+     ball_2 = BallTestDouble(position=vector(2 * ball_at_origin._ball.radius -.001, 0.0, 0.0))
+     assert_that(ball_at_origin.force_between(ball_2), is_not(vector(0, 0, 0)))
+
+  def test_momentum_for_zero_velocity(self, ball_at_origin):
+     assert_that(ball_at_origin.momentum(), equal_to(vector(0, 0, 0)))
+
+  def test_momentum_for_non_zero_velocity(self):
+     ball = BallTestDouble(velocity=vector(10, 0, 0))
+     assert_that(ball.momentum().x, equal_to(ball._ball.mass * ball._ball.velocity.x))
