b1727b020b7e24a739ebe43ce0ce7245c60c60ef
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-12-07 17:08:46 +0100

    Simplified code

--- src/demos/coriolis_force_on_eros.py
+++ src/demos/coriolis_force_on_eros.py
@@ -12,30 +12,28 @@ T = 5 # hours
 omega = 2 * pi / (T * 60 * 60)
 dt = 1
 
-eros = Eros()
 
 position_vector = vector(-EROS_RADIUS, 0,0 )
 velocity_vector = vector(-3, 3, 3)
 ball_mass = 1
 rotation_axis = vector(0, 1, 0)
 
+eros = Eros()
 ball = Ball(ball_mass, position=position_vector, velocity=velocity_vector, radius=EROS_RADIUS / 20,color=color.yellow, make_trail=True)
 
 t = 0
 while mag(ball.position) >= EROS_RADIUS:
   rate(1000)
   r = eros.distance_to(ball)
-
   W = omega * rotation_axis
-  force = -G * EROS_MASS * ball_mass * norm(r) / mag(r)**2 + ball_mass * cross(cross(W, r), W) - 2 * cross(W, ball.momentum)
-  
+  force = -G * EROS_MASS * ball_mass * norm(r) / mag(r)**2 + ball_mass * cross(cross(W, r), W) - 2 * cross(W, ball.momentum)  
   ball.move(force, dt)
   t += dt
 
-t = 0
 velocity_vector += cross(W, position_vector)
 ball_2 = Ball(ball_mass, position=position_vector, velocity=velocity_vector, radius=EROS_RADIUS / 20,color=color.magenta, make_trail=True)
 
+t = 0
 while mag(ball_2.position) >= EROS_RADIUS:
   rate(1000)
   r2 = eros.distance_to(ball_2)
