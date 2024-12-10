9ba5bac4b02b1c3d8af1a98d475805647555b030
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-12-10 19:06:53 +0100

    Code clean-up

--- src/demos/dipole.py
+++ src/demos/dipole.py
@@ -2,20 +2,25 @@
 # Original: https://github.com/Physics-Morris/Physics-Vpython/blob/master/7_Dipole.py
 #
 
-from vpython import canvas, vec, sphere, arrow, mag, hat, cos, sin, exp
+from vpython import canvas, vec
 from toolbox.charge import Charge, ec
 from toolbox.field import Field
 
+class Dipole:
+    def __init__(self):
+        radius = 1.2E-14
+        self._charges = []
+        self._charges.append(Charge(position=vec(10 * radius, 0, 0), radius=radius, coulomb=ec))
+        self._charges.append(Charge(position=vec(-10 * radius, 0, 0), radius=radius, coulomb=ec))
+
+    @property
+    def field(self):
+        return Field(self._charges)
+
 def main():
     scene = canvas(width=1000, height=600, align='left', range=3E-13)
-    charges = []
-    
-    radius = 1.2E-14
-    charges.append(Charge(position=vec(10 * radius, 0, 0), radius=radius, coulomb=ec))
-    charges.append(Charge(position=vec(-10 * radius, 0, 0), radius=radius, coulomb=ec))
-
-    field = Field(charges)
-    field.show(x_range=range(-22, 22, 5), y_range=range(-22, 22, 5), z_range=range(-12, 12, 5))
+    dipole = Dipole()
+    dipole.field.show(x_range=range(-22, 22, 5), y_range=range(-22, 22, 5), z_range=range(-12, 12, 5))
 
     while True:
         pass
