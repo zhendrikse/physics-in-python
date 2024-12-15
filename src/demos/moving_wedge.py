94116e074398b743607f0f414f061d6d0fe27e44
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-12-15 15:15:28 +0100

    Back to working version

--- src/demos/moving_wedge.py
+++ src/demos/moving_wedge.py
@@ -144,29 +144,27 @@ s2 = winput(bind=set_friction, type='numeric')
 scene.append_to_caption('<i>\n\n(Please press enter after setting each parameter, otherwise \n it will run on default parameter)</i>')
 scene.append_to_caption('\n\n\n\n\n')
 
-class Wedge:
-    def __init__(self):
-        A = vertex(pos=vec(0, 0, 0), color=color.orange, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
-        B = vertex(pos=vec(10/tan(radians(theta)), 0, 0), color=color.purple, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
-        C = vertex(pos=vec(10/tan(radians(theta)), 0, 10), color=color.green, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
-        D = vertex(pos=vec(0, 0, 10), color=color.blue, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
-        E = vertex(pos=vec(0, 10, 10), color=color.cyan, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
-        F = vertex(pos=vec(0, 10, 0), color=color.red, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
+def set_wedge():
 
-        self._apex = [A, B, C, D, E, F]
+    A = vertex(pos=vec(0, 0, 0), color=color.orange, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
+    B = vertex(pos=vec(10/tan(radians(theta)), 0, 0), color=color.purple, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
+    C = vertex(pos=vec(10/tan(radians(theta)), 0, 10), color=color.green, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
+    D = vertex(pos=vec(0, 0, 10), color=color.blue, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
+    E = vertex(pos=vec(0, 10, 10), color=color.cyan, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
+    F = vertex(pos=vec(0, 10, 0), color=color.red, v=vec(0, 0, 0), a=vec(accelerations.acceleration_wedge_x, 0, 0))
 
-        T1 = triangle(v0=E, v1=D, v2=C)
-        T2 = triangle(v0=F, v1=A, v2=B)
-        Q1 = quad(v0=F, v1=E, v2=D, v3=A)
-        Q2 = quad(v0=F, v1=E, v2=C, v3=B)
-        Q3 = quad(v0=A, v1=B, v2=C, v3=D)
+    apex = [A, B, C, D, E, F]
 
-    @property
-    def apex(self):
-        return self._apex
+    T1 = triangle(v0=E, v1=D, v2=C)
+    T2 = triangle(v0=F, v1=A, v2=B)
+    Q1 = quad(v0=F, v1=E, v2=D, v3=A)
+    Q2 = quad(v0=F, v1=E, v2=C, v3=B)
+    Q3 = quad(v0=A, v1=B, v2=C, v3=D)
 
-wedge = Wedge()
-apex = wedge.apex
+    return A, B, C, D, E, F
+
+A, B, C, D, E, F = set_wedge()
+apex = [A, B, C, D, E, F]
 
 floor = box(pos=vec(0,0,0), size=vec(300, 1, 30), color=color.blue, v=vec(0, 0, 0), 
             a=vec(0, 0, 0))
@@ -226,14 +224,15 @@ while True:
         else: tmp_m = s3.number
 
         if ball.pos.y >= ball.radius+floor.size.y/2:
-            p = ball.v.x * tmp_m + apex[0].v.x * tmp_M
+            p = ball.v.x * tmp_m + A.v.x * tmp_M
             m_v.plot(pos=(t, ball.v.x))
-            M_v.plot(pos=(t, apex[0].v.x))
+            M_v.plot(pos=(t, A.v.x))
 
             # m_p.plot(pos=(t, m * ball.v.x))
+
             # M_p.plot(pos=(t, s0.value * A.v.x))
 
-        K = 0.5*tmp_m*(ball.v.x**2 + ball.v.y**2) + 0.5*tmp_M* apex[0].v.x**2
+        K = 0.5*tmp_m*(ball.v.x**2 + ball.v.y**2) + 0.5*tmp_M*A.v.x**2
         U = tmp_m*grav_constant*(ball.pos.y - (ball.radius+floor.size.y/2))
 
         m_E.plot(pos=(t, K))
