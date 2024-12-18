dad22455be1a65998e96f52bebf3330730e4d6cb
Zeger Hendrikse <zegerh@yahoo.co.uk>
2024-12-18 18:59:22 +0100

    Code clean-up

--- src/toolbox/ball.py
+++ src/toolbox/ball.py
@@ -2,122 +2,129 @@ from vpython import vector, sphere, color, mag, norm
 
 zero_force = vector(0, 0, 0)
 
+
 class Ball:
-  def __init__(self, 
-               mass=1.5, 
-               position=vector(0, 0, 0), 
-               velocity=vector(0, 0, 0), 
-               radius=0.1, 
-               color=color.yellow, 
-               elasticity=1.0, 
-               make_trail=False, 
-               draw=True):
-    
-    self._ball = self._vpython_ball(mass, position, velocity, radius, color, elasticity, make_trail) if draw else None
-    self._mass = mass
-    self._radius = radius
-    self._elasticity = elasticity
-    self._velocity = velocity
-    self._elasticity = elasticity
-    self._position = position
+    def __init__(self,
+                 mass=1.5,
+                 position=vector(0, 0, 0),
+                 velocity=vector(0, 0, 0),
+                 radius=0.1,
+                 color=color.yellow,
+                 elasticity=1.0,
+                 make_trail=False,
+                 draw=True):
 
-  def _vpython_ball(self, mass, position, velocity, radius, color, elasticity, make_trail):
-      return sphere(pos=position, radius=radius, color=color, velocity=velocity, mass=mass, elasticity=elasticity, make_trail=make_trail)
-  
-  def _draw(self):
-      if self._ball:
-          self._ball.pos = self._position
-          self._ball.velocity = self._velocity
+        self._ball = self._vpython_ball(mass, position, velocity, radius, color, elasticity,
+                                        make_trail) if draw else None
+        self._mass = mass
+        self._radius = radius
+        self._elasticity = elasticity
+        self._velocity = velocity
+        self._elasticity = elasticity
+        self._position = position
 
-  def move(self, force__vector=vector(0, 0, 0), dt=0.01):
-      # Newton's second law: F = m * a
-      acceleration_vector = force__vector / self.mass
-      self._velocity += acceleration_vector * dt
-      self._position += self._velocity * dt
-      self._draw()
-    
-  def shift(self, delta):
-      self._position += delta
-      self._draw()
-  
-  def force_between(self, other_ball):
-      if not self.has_collided_with(other_ball):
-         return zero_force
-      
-      k = 101
-      r = self.distance_to(other_ball)
-      return k * (mag(r) - (self.radius + other_ball.radius)) * norm(r)
-  
-  def distance_to(self, other):
-     return other.position - self.position
-    
-  def has_collided_with(self, other_ball):
-    return mag(self.distance_to(other_ball)) < (self.radius + other_ball.radius)
-    
-  def rotate(self, angle, origin=vector(0, 0, 0), axis=vector(0, 1, 0)):
-    self._ball.rotate(origin=origin, axis=axis, angle=angle)
+    @staticmethod
+    def _vpython_ball(mass, position, velocity, radius, color, elasticity, make_trail):
+        return sphere(pos=position, radius=radius, color=color, velocity=velocity, mass=mass, elasticity=elasticity,
+                      make_trail=make_trail)
 
-  def lies_on_floor(self):
-    return self.position.y - self.radius <= 0.5
-  
-  def bounce_from_floor(self, dt):
-    self._velocity.y *= -self.elasticity
-    self._position   += self._velocity * dt
+    def _draw(self):
+        if self._ball:
+            self._ball.pos = self._position
+            self._ball.velocity = self._velocity
 
-    # if the velocity is too slow, stay on the ground
-    if self._velocity.y <= 0.1:
-        self._position.y = self.radius + self.radius / 10
-    
-    self._draw()
+    def move(self, force__vector=vector(0, 0, 0), dt=0.01):
+        # Newton's second law: F = m * a
+        acceleration_vector = force__vector / self.mass
+        self._velocity += acceleration_vector * dt
+        self._position += self._velocity * dt
+        self._draw()
 
-  def _is_approaching_from_the_right(self, right_side):
-     return self.velocity.x < 0 and self.position.x > right_side
+    def shift(self, delta):
+        self._position += delta
+        self._draw()
 
-  def _is_approaching_from_the_left(self, left_side):
-     return self.velocity.x > 0 and self.position.x < left_side
+    def force_between(self, other_ball):
+        if not self.has_collided_with(other_ball):
+            return zero_force
 
-  def hits(self, building):
-      right_side = building.position.x + building.L / 2
-      left_side = building.position.x - building.L / 2
+        k = 101
+        r = self.distance_to(other_ball)
+        return k * (mag(r) - (self.radius + other_ball.radius)) * norm(r)
 
-      if self._is_approaching_from_the_right(right_side):
-        return self.position.x <= right_side + self.radius and self.position.y <= building.H    
-      
-      if self._is_approaching_from_the_left(left_side):
-        return self.position.x >= left_side - self.radius and self.position.y <= building.H 
-      
-      return False
-  
-  def collide_with(self, building):
-    building.collide_with(self)
-    # set new velocity in x-direction after collision with building
-    self._velocity.x = (self.mass * self.velocity.x + building.mass * building.velocity.x + self.elasticity * building.mass * (building.velocity.x - self.velocity.x))/(self.mass + building.mass)
-    self._draw()
-  
-  @property
-  def momentum(self):
-     return self.velocity * self.mass
-  
-  @property
-  def kinetic_energy(self):
-     return mag(self.momentum) * mag(self.momentum) / (2 * self.mass)
-  
-  @property
-  def position(self):
-    return self._position
-  
-  @property
-  def velocity(self):
-    return self._velocity
-  
-  @property
-  def mass(self):
-     return self._mass
-  
-  @property
-  def radius(self):
-     return self._radius
-  
-  @property
-  def elasticity(self):
-     return self._elasticity
+    def distance_to(self, other):
+        return other.position - self.position
+
+    def has_collided_with(self, other_ball):
+        return mag(self.distance_to(other_ball)) < (self.radius + other_ball.radius)
+
+    def rotate(self, angle, origin=vector(0, 0, 0), axis=vector(0, 1, 0)):
+        self._ball.rotate(origin=origin, axis=axis, angle=angle)
+
+    def lies_on_floor(self):
+        return self.position.y - self.radius <= 0.5
+
+    def bounce_from_floor(self, dt):
+        self._velocity.y *= -self.elasticity
+        self._position += self._velocity * dt
+
+        # if the velocity is too slow, stay on the ground
+        if self._velocity.y <= 0.1:
+            self._position.y = self.radius + self.radius / 10
+
+        self._draw()
+
+    def _is_approaching_from_the_right(self, right_side):
+        return self.velocity.x < 0 and self.position.x > right_side
+
+    def _is_approaching_from_the_left(self, left_side):
+        return self.velocity.x > 0 and self.position.x < left_side
+
+    def hits(self, building):
+        right_side = building.position.x + building.L / 2
+        left_side = building.position.x - building.L / 2
+
+        if self._is_approaching_from_the_right(right_side):
+            return self.position.x <= right_side + self.radius and self.position.y <= building.H
+
+        if self._is_approaching_from_the_left(left_side):
+            return self.position.x >= left_side - self.radius and self.position.y <= building.H
+
+        return False
+
+    def collide_with(self, building):
+        building.collide_with(self)
+        # set new velocity in x-direction after collision with building
+        momentum_ball = self.mass * self.velocity.x
+        momentum_building = building.mass * building.velocity.x
+        self._velocity.x = (momentum_ball + momentum_building + self.elasticity * building.mass * (
+                    building.velocity.x - self.velocity.x)) / (self.mass + building.mass)
+        self._draw()
+
+    @property
+    def momentum(self):
+        return self.velocity * self.mass
+
+    @property
+    def kinetic_energy(self):
+        return mag(self.momentum) * mag(self.momentum) / (2 * self.mass)
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    @property
+    def mass(self):
+        return self._mass
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @property
+    def elasticity(self):
+        return self._elasticity
--- src/toolbox/building.py
+++ src/toolbox/building.py
@@ -2,23 +2,25 @@ from vpython import box, vec, color
 
 g = 98
 
+
 class Building:
-    def __init__(self, 
-                 mass=10, 
-                 position=vec(0, 0, 0), 
-                 length=20, 
-                 height=100, 
-                 width=50, 
-                 color=color.orange, 
-                 up=vec(0, 1, 0), 
-                 velocity=vec(0, 0, 0), 
-                 omega=0, 
+    def __init__(self,
+                 mass=10,
+                 position=vec(0, 0, 0),
+                 length=20,
+                 height=100,
+                 width=50,
+                 color=color.orange,
+                 up=vec(0, 1, 0),
+                 velocity=vec(0, 0, 0),
+                 omega=0,
                  draw=True):
-        
-        self._building = self._vpython_box(mass, position, length, height, width, color, up, velocity, omega) if draw else None
+
+        self._building = self._vpython_box(mass, position, length, height, width, color, up, velocity,
+                                           omega) if draw else None
         self._position_in_rest = position
         self._position = position
-        self._length  = length
+        self._length = length
         self._height = height
         self._width = width
         self._velocity = velocity
@@ -26,55 +28,58 @@ class Building:
         self._w = omega
         self._mass = mass
 
-    def _vpython_box(self, mass, position, length, height, width, color, up, velocity, w):
-        return box(mass=mass, pos=position, length=length, height=height, width=width, color=color, up=up, velocity=velocity, w=w)
+    @staticmethod
+    def _vpython_box(mass, position, length, height, width, color, up, velocity, w):
+        return box(mass=mass, pos=position, length=length, height=height, width=width, color=color, up=up,
+                   velocity=velocity, w=w)
 
     def _angular_acceleration(self):
         if self.position == self._position_in_rest:
             return 0
-        
+
         center = self._position.x
         r = abs(self._position.x + self.H + self.L / 2)
         omega = g * r / self.moment_of_inertia
         if -self.H - center > 10:
-            return -omega # backward_rotation
+            return -omega  # backward_rotation
         elif -self.H - center <= 10:
-            return omega # forward rotation
+            return omega  # forward rotation
         else:
             return 0
-            
+
     def collide_with(self, ball):
         # sets angular velocity omega after collision with ball
-        velocity = (ball.mass * ball.velocity.x + self.mass * self.velocity.x + ball.elasticity * ball.mass * (ball.velocity.x - self.velocity.x))/(ball.mass + self.mass)
+        velocity = (ball.mass * ball.velocity.x + self.mass * self.velocity.x + ball.elasticity * ball.mass * (
+                    ball.velocity.x - self.velocity.x)) / (ball.mass + self.mass)
         radius = ball.position.y - 0.5
         omega = velocity / radius
         self._w = omega
 
     def _lies_flat_on_ground(self):
-        return self.position.y <= self.L / 2 +.5
-    
+        return self.position.y <= self.L / 2 + .5
+
     def update(self, dt):
         self._w += self._angular_acceleration() * dt
         dtheta = -self._w * dt
-        
+
         if self._lies_flat_on_ground():
             self._w = 0
             self._up = vec(-1, 0, 0)
             dtheta = 0
 
-        self.rotate(dtheta) 
+        self.rotate(dtheta)
 
     def rotate(self, dtheta):
-        if self._building:    
+        if self._building:
             self._building.up = self._up
             self._building.rotate(origin=vec(-self.L / 2 - self.H, 0, 0), axis=vec(0, 0, 1), angle=dtheta)
             self._up = self._building.up
             self._position = self._building.pos
-       
+
     @property
     def mass(self):
         return self._mass
-    
+
     @property
     def velocity(self):
         return self._velocity
@@ -82,32 +87,31 @@ class Building:
     @property
     def H(self):
         return self._height
-    
+
     @property
     def position(self):
         return self._position
-    
+
     @property
     def L(self):
         return self._length
-    
+
     @property
     def omega(self):
         return self._w
 
-    @property 
+    @property
     def W(self):
         return self._width
-    
+
     @property
     def up(self):
         return self._up
-        
+
     @property
     def moment_of_inertia(self):
         # I = m / 12 * (H * H + L * L), see https://kids.kiddle.co/Moment_of_inertia
-        H = self.H 
-        L = self.L
+        h = self.H
+        l = self.L
         m = self.mass
-        return m / 12 * (H * H + L * L)
-    
+        return m / 12 * (h * h + l * l)
