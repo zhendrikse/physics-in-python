99a08b65c2882dbb98d0bf4dc25ce728781f1206
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-01-05 16:39:45 +0100

    Updated charged rings

--- run_glowscript.sh
+++ run_glowscript.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 1 ]; then
     echo $'Give name of module to be executed as parameter:\n$ run_demo.sh <python_module_name>\n'
-    echo $'For example: ./run_demo.sh bouncing_ball'
+    echo $'For example: ./run_glowscript.sh bouncing_ball'
     exit 0
 fi
 
--- src/glowscript/electromagnetism/charged_rings.py
+++ src/glowscript/electromagnetism/charged_rings.py
@@ -1,41 +1,20 @@
 #Web VPython 3.2
-from vpython import scene, arange, pi, sphere, vector, sin, cos, ring, vec, arrow, color, mag, norm, rate
-
+from vpython import canvas, arange, pi, sphere, vector, sin, cos, ring, vec, arrow, color, mag, norm, rate, checkbox
 
 title = """Electric field inside charged rings. 
 
 &#x2022; Original <a href="https://lectdemo.github.io/virtual/18-Erings.html">18-Erings.html</a>
 &#x2022; Maintained by <a href="https://github.com/zhendrikse/">Zeger Hendrikse</a> in this <a href="https://github.com/zhendrikse/physics-in-python/">GitHub repository</a>
-&#x2022; &lt;f&gt; &rarr; toggle electric field arrows
 &#x2022; &lt;s&gt; &rarr; screenshot
 &#x2022; &lt;v&gt; &rarr; verbose output
-&#x2022; &lt;space&gt; &rarr; toggle background color
-&#x2022; &lt;mouse click&gt; &rarr; toggle display of ring charges
 
 """
+
+animation = canvas(width="1024", height="768", title=title, background=color.black, forward=vec(0.18, 0.35, -0.95))
 
 scale_factor = 2e-11
 L = 6e-3
 dL = 1e-3
-
-def hsv_to_rgb(hue, saturation, value):
-    if saturation:
-        if hue == 1.0: hue = 0.0
-        i = int(hue * 6.0)
-        f = hue * 6.0 - i
-
-        w = value * (1.0 - saturation)
-        q = value * (1.0 - saturation * f)
-        t = value * (1.0 - saturation * (1.0 - f))
-
-        if i == 0: return value, t, w
-        if i == 1: return q, value, w
-        if i == 2: return w, value, t
-        if i == 3: return w, q, value
-        if i == 4: return t, w, value
-        if i == 5: return value, w, q
-    else:
-        return value, value, value
 
 class ChargedRing:
     def __init__(self, ring_radius=10., point_charges_amount=20, totalq=1e-9, xx=0.):
@@ -53,14 +32,13 @@ class ChargedRing:
             elif totalq < 0:
                 hue = (240 / 360)
             elif totalq == 0:
-                sat = 0
-            rgb = hsv_to_rgb(hue, saturation, 1.0)
-            charge.color = vec(rgb[0], rgb[1], rgb[2])
+                saturation = 0
+            charge.color = color.hsv_to_rgb(vec(hue, saturation, 1.0))
             ring(pos=vector(xx, 0, 0), axis=vector(1, 0, 0), radius=ring_radius, thickness=ring_radius / 75., color=charge.color)
 
-    def toggle_show_charges(self):
+    def visible_is(self, visible):
         for charge in self._charges:
-            charge.visible = not charge.visible
+            charge.visible = visible
 
     def electric_field_at(self, location):
         E = vector(0, 0, 0)
@@ -77,65 +55,67 @@ class Field:
     def __init__(self, rings):
         self._field_arrows = []
         for x in arange((-L + dL), L - dL, dL / 2):
-            for y in arange(-(2 / 3) * rings[0].radius(), rings[0].radius(), rings[0].radius() / 3.):
+            for y in arange(-(2 / 3) * rings.radius(), rings.radius(), rings.radius() / 3.):
                 location = vector(x, y, 0)
-                E = vector(0, 0, 0)
-                for a_ring in rings:
-                    E += a_ring.electric_field_at(location)
-                self._field_arrows += [arrow(pos=location, axis=E * scale_factor, color=color.cyan, shaftwidth=rings[0].radius() / 20.)]
+                E = rings.field_at(location)
+                self._field_arrows += [arrow(pos=location, axis=E * scale_factor, color=color.cyan, shaftwidth=rings.radius() / 20.)]
 
-    def toggle_show_field(self):
+    def visible_is(self, visible):
         for field_arrow in self._field_arrows:
-            field_arrow.visible = not field_arrow.visible
+            field_arrow.visible = visible
 
-def create_rings(dQ=5e-10):
-    ##for x in arange (-L,1.1*L,dL):
-    count = 0
-    rings = []
-    total_charge = -3 * dQ # Charge of left ring
-    for x in arange(-2 * L, 2.5 * L, dL):
-        rings.append(ChargedRing(ring_radius=2e-3, point_charges_amount=20, totalq=total_charge, xx=x))
-        print('charge of ring #', count, "=", total_charge)
-        total_charge = total_charge + dQ
-        count += 1
-    return rings
+class Rings:
+    def __init__(self, ring_radius=2e-3, dQ=5e-10):
+        ##for x in arange (-L,1.1*L,dL):
+        self._radius = ring_radius
+        self._rings = []
+        total_charge = -3 * dQ # Charge of left ring
+        count = 0
+        for x in arange(-2 * L, 2.5 * L, dL):
+            self._rings.append(ChargedRing(ring_radius, point_charges_amount=20, totalq=total_charge, xx=x))
+            total_charge = total_charge + dQ
+            count += 1
 
-scene.width = 1024
-scene.height = 768
-scene.title = title
-scene.background = color.black
-scene.forward=vec(0.18, 0.35, -0.95)
+    def field_at(self, location):
+        field = vec(0, 0, 0)
+        for a_ring in self._rings:
+            field += a_ring.electric_field_at(location)
+        return field
 
-rings = create_rings()
-field = Field(rings)
+    def charges_visible_is(self, visible):
+        for ring in self._rings:
+            ring.visible_is(visible)
 
-def toggle_background():
-    scene.background = color.white if scene.background is color.black else color.black
+    def radius(self):
+        return self._radius
 
-def toggle_field():
-    field.toggle_show_field()
+rings = Rings(ring_radius=2e-3)
+electric_field = Field(rings)
+
+def toggle_charges(event):
+    rings.charges_visible_is(event.checked)
+
+def toggle_field(event):
+    electric_field.visible_is(event.checked)
+
+def toggle_background(event):
+    animation.background = color.black if event.checked else color.white
 
 def on_key_press(event):
-    if event.key == "f":
-        toggle_field()
-    if event.key == " ":
-        toggle_background()
     if event.key == 's':
-        scene.capture("electric_field_of_charged_rings")
+        animation.capture("electric_field_of_charged_rings")
     if event.key == 'v':
-        print("scene.center=" + str(scene.center))
-        print("scene.forward=" + str(scene.forward))
-        print("scene.range=" + str(scene.range))
+        print("scene.center=" + str(animation.center))
+        print("scene.forward=" + str(animation.forward))
+        print("scene.range=" + str(animation.range))
 
-def on_mouse_click():
-    for ring in rings:
-        ring.toggle_show_charges()
+animation.append_to_caption("\n")
+background_box = checkbox(text="Dark background", checked=True, bind=toggle_background)
+field_box = checkbox(text="Show field", checked=True, bind=toggle_field)
+charges_box = checkbox(text="Show charges", checked=True, bind=toggle_charges)
+animation.bind("keydown", on_key_press)
 
-
-scene.bind("keydown", on_key_press)
-scene.bind("click", on_mouse_click)
-
-while 1:
+while True:
     rate(50)
 
 
