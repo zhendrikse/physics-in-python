85bbce8d1cdcd1766acec19e439ceb912902138b
Zeger Hendrikse <zegerh@yahoo.co.uk>
2025-01-26 11:20:57 +0100

    First working version

--- src/mathematics/3d_plots.py
+++ src/mathematics/3d_plots.py
@@ -1,91 +1,36 @@
 # Web VPython 3.2
 
 from vpython import *
+import numpy as np
 
-# https://github.com/nicolaspanel/numjs
-get_library('https://cdnjs.cloudflare.com/ajax/libs/numjs/0.16.1/numjs.min.js')
-# get_library("https://cdnjs.cloudflare.com/ajax/libs/mathjs/14.0.1/math.js")
+animation = canvas()
 
-animation = canvas(background=color.gray(0.1))
+class NumpyWrapper:
+    def __init__(self, resolution, start, stop):
+        self._resolution = resolution
+        x = y = np.linspace(-2 * pi, 2 * pi, resolution)
+        self._x, self._y = np.meshgrid(x, y)
 
+    @staticmethod
+    def _convert_back_to_python_array(numpy_array):
+        result = []
+        for x in range(numpy_array.shape[0]):
+            temp = []
+            for y in range(numpy_array.shape[1]):
+                temp += [numpy_array[x, y]]
+            result += [temp]
 
-class Numpy:
-    def __init__(self):
-        self.array = self._array
-        self.linspace = self._linspace
-        self.len = self._len
-        self.meshgrid = self._meshgrid
-        self.concatenate = self._concatenate
-        self.sqrt = self._sqrt
-        self.cos = self._cos
-        self.sin = self._sin
-        self.tan = self._tan
-        self.exp = self._exp
-        self.log = self._log
-        self.abs = self._abs
-        self.ones = self._ones
-        self.zeros = self._zeros
-        self.random = self._random
+        return result
 
-    def _abs(self, numpy_array):
-        return nj.abs(numpy_array)
-
-    def _exp(self, numpy_array):
-        return nj.exp(numpy_array)
-
-    def _log(self, numpy_array):
-        return nj.log(numpy_array)
-
-    def _cos(self, numpy_array):
-        return nj.cos(numpy_array)
-
-    def _sin(self, numpy_array):
-        return nj.sin(numpy_array)
-
-    def _tan(self, numpy_array):
-        return nj.tan(numpy_array)
-
-    def _sqrt(self, numpy_array):
-        return nj.sqrt(numpy_array)
-
-    def _len(self, numpy_array):
-        return numpy_array.shape[0]
-
-    def _array(self, an_array):
-        return nj.array(an_array)
-
-    def _ones(self, shape_array):
-        return nj.ones(shape_array)
-
-    def _zeros(self, shape_array):
-        return nj.zeros(shape_array)
-
-    def _random(self, shape_array):
-        return nj.random(shape_array)
-
-    def _concatenate(self, numpy_array_1, numpy_array_2):
-        return nj.concatenate(numpy_array_1, numpy_array_2)
-
-    def _linspace(self, start, stop, num):
-        return self._array([x for x in arange(start, stop, (stop - start) / (num - 1))] + [stop])
-
-    def _meshgrid(self, linspace_1, linspace_2):
-        xx = nj.stack([linspace_1 for _ in range(linspace_1.shape)])
-        temp = []
-        for i in range(linspace_2.shape[0]):
-            for j in range(linspace_2.shape[0]):
-                temp.append(linspace_2.get(i))
-        yy = nj.array(temp).reshape(linspace_2.shape[0], linspace_2.shape[0])
-        return xx, yy
-
-
-np = Numpy()
-
+    def get_x_y(self):
+        x = self._convert_back_to_python_array(self._x)
+        y = self._convert_back_to_python_array(self._y)
+        return x, y
 
 # This class is only meant to be used from within the Figure class.
 class SubPlot:
-    def __init__(self, xx, yy, zz):
-        self._xx, self._yy, self._zz = xx, yy, zz
+    def __init__(self, xx, yy, f_x_y_t):
+        self._xx, self._yy, self._f_x_y_t = xx, yy, f_x_y_t
         self._hue_offset = .2
         self._opacity = 1
         self._shininess = 0.6
@@ -103,20 +48,20 @@ class SubPlot:
             vertex_.visible = False
 
     def _create_vertex(self, x, y):
-        x_ = self._xx.get(x, y) * self._xx.shape[0]
-        y_ = self._yy.get(x, y) * self._yy.shape[1]
+        x_ = self._xx[x][y] * len(self._xx)
+        y_ = self._yy[x][y] * len(self._yy[0])
         self._vertices.append(vertex(pos=vec(x_, 0, y_), normal=vec(0, 1, 0)))
 
     def _create_vertices(self):
-        for x in range(self._xx.shape[0]):
-            for y in range(self._yy.shape[1]):
+        for x in range(len(self._xx)):
+            for y in range(len(self._yy[1])):
                 self._create_vertex(x, y)
 
     def _create_quad(self, x, y):
         _neighbor_increment_x, _neighbor_increment_y = 1, 1
-        if x == (self._xx.shape[0] - 1):
+        if x == (len(self._xx) - 1):
             _neighbor_increment_x = -x
-        if y == (self._yy.shape[1] - 1):
+        if y == (len(self._yy[0]) - 1):
             _neighbor_increment_y = -y
 
         v0 = self._get_vertex(x, y)
@@ -127,15 +72,15 @@ class SubPlot:
 
     # Create the quad objects, based on the vertex objects already created.
     def _create_quads(self):
-        for x in range(self._xx.shape[0] - 1):
-            for y in range(self._yy.shape[1] - 1):
+        for x in range(len(self._xx) - 1):
+            for y in range(len(self._yy[0]) - 1):
                 self._create_quad(x, y)
 
     def _set_vertex_normal_for(self, x, y):
         _neighbor_increment_x, _neighbor_increment_y = 1, 1
-        if x == (self._xx.shape[0] - 1):
+        if x == (len(self._xx) - 1):
             _neighbor_increment_x = -x
-        if y == (self._yy.shape[1] - 1):
+        if y == (len(self._yy[0]) - 1):
             _neighbor_increment_y = -y
         vertex_ = self._get_vertex(x, y)
         #        normal_total_ = self._get_vertex(x, y + _neighbor_increment_y).normal
@@ -153,12 +98,12 @@ class SubPlot:
     # Set the normal for each vertex to be perpendicular to the lower left corner of the quad.
     # The vectors a and b point to the right and up around a vertex in the xy plane.
     def _make_normals(self):
-        for x in range(self._xx.shape[0]):
-            for y in range(self._yy.shape[1]):
+        for x in range(len(self._xx)):
+            for y in range(len(self._yy[1])):
                 self._set_vertex_normal_for(x, y)
 
     def _update_vertex(self, x, y, value):
-        hue = .5 / self._yy.shape[1] * self._hue_gradient * abs(value) + self._hue_offset
+        hue = .5 / len(self._yy[0]) * self._hue_gradient * abs(value) + self._hue_offset
         vertex_ = self._get_vertex(x, y)
         vertex_.pos.y = value
         vertex_.color = color.hsv_to_rgb(vec(hue, 1, 1))
@@ -166,14 +111,14 @@ class SubPlot:
         vertex_.shininess = self._shininess
 
     def _update_vertices(self, t):
-        for x in range(self._xx.shape[0]):
-            for y in range(self._yy.shape[1]):
+        for x in range(len(self._xx)):
+            for y in range(len(self._yy[1])):
                 f_x_y = self._zz.get(x, y) * (cos(self._omega * t) + 1) * .5
                 value = self._zz.shape[0] * f_x_y
                 self._update_vertex(x, y, value)
 
     def _get_vertex(self, x, y):
-        return self._vertices[x * self._yy.shape[1] + y]
+        return self._vertices[x * len(self._yy[0]) + y]
 
     def render(self, t):
         self._update_vertices(t)
@@ -194,20 +139,17 @@ class SubPlot:
     def set_shininess_to(self, shininess):
         self._shininess = shininess
 
-
-class SubPlotFxyt(SubPlot):
+class MultivariateFunctionPlot(SubPlot):
     def __init__(self, xx, yy, f_x_y_t):
-        SubPlot.__init__(self, xx, yy, None)
-        self._f_x_y_t = f_x_y_t
+        super().__init__(xx, yy, f_x_y_t)
 
     def _update_vertices(self, t):
-        for x in range(self._xx.shape[0]):
-            for y in range(self._yy.shape[1]):
-                x_ = self._xx.get(x, y)
-                y_ = self._yy.get(x, y)
-                value = self._xx.shape[0] * self._f_x_y_t(x_, y_, t)
+        for x in range(len(self._xx)):
+            for y in range(len(self._yy[0])):
+                x_ = self._xx[x][y]
+                y_ = self._yy[x][y]
+                value = len(self._xx) * self._f_x_y_t(x_, y_, self._omega * t)
                 self._update_vertex(x, y, value)
-
 
 class Figure:
     """
@@ -324,13 +266,6 @@ shininess_slider = slider(min=0, max=1, step=0.01, value=0.6, bind=adjust_shinin
 
 animation.append_to_caption("\n\n")
 
-resolution = 50
-x = y = np.linspace(-2 * pi, 2 * pi, resolution)
-x, y = np.meshgrid(x, y)
-
-
-def f_x_y_t(x, y, t):
-    return sin(sqrt(x * x + y * y)) * (cos(t) + 1)
 
 
 # resolution = 50
@@ -342,12 +277,16 @@ def f_x_y_t(x, y, t):
 # y = np.sin(theta).add(np.cos(phi))
 # z = np.sin(phi).multiply(3)
 
+xx, yy = NumpyWrapper(50, -pi, pi).get_x_y()
+
+def f_x_y_t(x, y, t):
+    return sin(sqrt(x * x + y * y)) * (cos(t) + 1)
+
 
 figure = Figure()
-# plot = figure.add_subplot(x, y, z, type="f(x,y,z)")
-figure.add_subplot(SubPlotFxyt(x, y, f_x_y_t))
+figure.add_subplot(MultivariateFunctionPlot(xx, yy, f_x_y_t))
 
-dt = 0.05
+dt = 0.01
 time = 0
 while True:
     rate(30)
